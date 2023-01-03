bd9c4710e5b3dc8e236e8ce92c0504dfaa35ae4b
Chelsea S <workstation-chelsea@Chelseas-MacBook-Pro.local>
2023-01-03 14:55:41 -0500

    uploading functions homework

--- js/exercises.js
+++ js/exercises.js
@@ -4,26 +4,60 @@
   // ---------------------
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
-
-
+  
+  //function determines if x or y is larger
+  
+  function max(num1, num2) {
+     if (num1 > num2) {
+       return num1;
+   } else {
+     return num2;
+   }
+    console.log(num1 || num2);
+  };
+ // if x > y than print x. If not, print y.
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
 
+  function maxOfThree(num1, num2, num3) {
+    if (num1 > num2) {
+      return num1;
+    } if (num1 < num2) {
+      return num2; 
+    } if (num1 < num3) {  
+      return num3;
+     } if (num2 > num3) {
+      return num2;
+    } else {
+      return num3;
+    }
+    }
 
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
 
-
+    function isVowel(vowel) {
+     const comparison = [];
+     if (vowel === comparison ) {
+      return true;
+     } else {
+      return false;
+     }
+    }
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
 
+    function rovarspraket(word) {
+      let word = "rovarspraket";
+      
 
+    }
 
   // ---------------------
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
